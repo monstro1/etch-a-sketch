30e65ae00f679ab92947bd6769fd8f576a9c2bd7
monstro1 <michael@monstro1.com>
2023-10-20 12:56:12 -0400

    Write for loop to create row divs

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const gridContainer = document.querySelector("#grid-container");
+
+for (let i = 0; i < 16; i++) {
+	let rowDiv = document.createElement("div");
+	rowDiv.classList.add("row");
+	gridContainer.appendChild(rowDiv);
+}
