77b614b518476a6674f8dd65ea1a2130a4203564
monstro1 <michael@monstro1.com>
2023-10-20 12:57:08 -0400

    Add inner for loop to create grid boxes
    The boxes are added to each row div

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ for (let i = 0; i < 16; i++) {
 	let rowDiv = document.createElement("div");
 	rowDiv.classList.add("row");
 	gridContainer.appendChild(rowDiv);
+	for (let j = 0; j < 16; j++) {
+		let gridBox = document.createElement("div");
+		rowDiv.appendChild(gridBox);
+	}
 }
