19c1126166e532c636ea7dcd8eadc01f9578fb4e
monstro1 <michael@monstro1.com>
2023-10-20 13:44:59 -0400

    Add event listener to grid boxes

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ for (let i = 0; i < 16; i++) {
 	for (let j = 0; j < 16; j++) {
 		let gridBox = document.createElement("div");
 		rowDiv.appendChild(gridBox);
+		gridBox.addEventListener("mouseover", function() {
+			gridBox.classList.add("filled");
+		});
 	}
 }
